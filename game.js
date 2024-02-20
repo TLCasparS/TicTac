c3cde3cff29bd97ce6e471dea8d12ae67dfc900e
Caspar <Tillman.Schlageter@web.de>
2024-02-20 19:50:37 +0100

    Build game board
    Array and player as objects

--- game.js
+++ game.js
@@ -1,1 +1,28 @@
 alert("tic tac toe")
+
+
+
+
+function makeBoard(){
+
+
+    let board = []
+
+    for (let  i = 0; i<3; i++){
+        let row = [];
+        for( let  j = 0; j<3; j++){
+            row.push(0);
+        }
+        board.push(row);
+    }
+    
+    return { board }
+
+}
+
+function createPlayer(name){
+    return { name}
+}
+
+gameBoard = makeBoard();
+console.log(gameBoard)
