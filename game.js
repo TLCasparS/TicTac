84fe5943421f87eae16be8d3c96dad4cd9583495
Caspar <Tillman.Schlageter@web.de>
2024-02-20 20:14:47 +0100

    Add plan for game
    write pseudo code to understand logic

--- game.js
+++ game.js
@@ -19,14 +19,38 @@ const makeBoard = (function(){
 
 
 
-function createPlayer(name){
+function createPlayer(name, symbol){
     let score = 0;
+    symbol = symbol[0]
 
     const giveScore = () => score;
     const scored = () => score++;
 
-    return { name, giveScore, scored}
+    return { name, symbol, giveScore, scored}
 }
 
+// gameloop
+// as long as noone one or lost the game is going on
+
+//each round 
+// player 1 picks where to place his symbol
+// check if place is free
+//  if not repeat to ask player 1
+// if free put in array
+// show game board
+// check for winners or gameboard full
+
+// player 2 turn 
+// player 2 picks where to place his symbol
+// check if place is free
+//  if not repeat to ask player 2
+// if free put in array
+// show game board
+// check for winners or gameboard full
+
+// 
+
 gameBoard = makeBoard();
-console.log(gameBoard)
+p1 = createPlayer("Jean", "J")
+p2 = createPlayer("Caspar", "C")
+console.log(gameBoard, p1.name, p2.symbol)
